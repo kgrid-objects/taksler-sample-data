811c9dd4c1c2f0b10ee54f7171d60e84da76a12b
gqmeng <gqmeng.meng@gmail.com>
2020-09-25 14:47:09 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,48 +1,79 @@
 function getData(inputs){
-  return JSON.stringify([{value: 9.5,
-    title: 'Do all preventative measures',
-    difficulty: 'HARD',
-    description: 'Do everything  your doctor  tells you to do'},
-    {value: 8.5,
-      title: 'Go Skydiving',
-      difficulty: 'EASY',
-      description: 'Just jump out of  a moving plane'},
-    {value: 6,
-      title: 'Bake a cake',
-      difficulty: 'MEDIUM',
-      description: 'You gotta do the cooking  by the book'},
-    {value: 5.2,
-      title: 'Fly to England',
-      difficulty: 'MEDIUM',
-      description: 'pack your bags and board a plane to UK'},
-    {value: 4,
-      title: 'Eat a sandwich',
-      difficulty: 'HARD',
-      description: 'selecting the ingredients is the hardest part'},
-    {value: 4,
-      title: 'Learn to juggle',
-      difficulty: 'EASY',
-      description: 'no excuses, literally everyone can juggle'},
-    {value: 3,
-      title: 'Quit smoking',
-      difficulty: 'HARD',
-      description: 'just go cold turkey, or wear a no smoking sign'},
-    {value: 2,
-      title: 'Quit drinking',
-      difficulty: 'MEDIUM',
-      description: 'not everything, you should still drink water'},
-    {value: 1,
-      title: 'Join a club',
-      difficulty: 'EASY',
-      description: 'extreme bingo meets on wednesdays'},
-    {value: 0.5,
-      title: 'Take up fishing',
-      difficulty: 'EASY',
-      description: 'just catch and release'},
-    {value: 0.33,
-      title: 'Comb your hair',
-      difficulty: 'HARD',
-      description: 'the direction is very important'}
-  ])
+  return JSON.stringify([
+    {
+        value: 9.5,
+        title: 'Do all preventative measures',
+        difficulty: 'HARD',
+        description: 'Do everything  your doctor  tells you to do',
+        color: "salmon"
+    },
+    {
+        value: 8.5,
+        title: 'Go Skydiving',
+        difficulty: 'EASY',
+        description: 'Just jump out of  a moving plane',
+        color: "Chartreuse"
+    },
+    {
+        value: 6,
+        title: 'Bake a cake',
+        difficulty: 'MEDIUM',
+        description: 'You gotta do the cooking  by the book',
+        color: "DarkCyan"
+    },
+    {
+        value: 5.2,
+        title: 'Fly to England',
+        difficulty: 'MEDIUM',
+        description: 'pack your bags and board a plane to UK',
+        color: "DarkSeaGreen"
+    },
+    {
+        value: 4,
+        title: 'Eat a sandwich',
+        difficulty: 'HARD',
+        description: 'selecting the ingredients is the hardest part',
+        color: "DodgerBlue"
+    },
+    {
+        value: 4,
+        title: 'Learn to juggle',
+        difficulty: 'EASY',
+        description: 'no excuses, literally everyone can juggle',
+        color: "ForestGreen"
+    },
+    {
+        value: 3,
+        title: 'Quit smoking',
+        difficulty: 'HARD',
+        description: 'just go cold turkey, or wear a no smoking sign',
+    },
+    {
+        value: 2,
+        title: 'Quit drinking',
+        difficulty: 'MEDIUM',
+        description: 'not everything, you should still drink water',
+    },
+    {
+        value: 1,
+        title: 'Join a club',
+        difficulty: 'EASY',
+        description: 'extreme bingo meets on wednesdays',
+        color: "IndianRed"
+    },
+    {
+        value: 0.5,
+        title: 'Take up fishing',
+        difficulty: 'EASY',
+        description: 'just catch and release',
+    },
+    {
+        value: 0.33,
+        title: 'Comb your hair',
+        difficulty: 'HARD',
+        description: 'the direction is very important',
+        color: "Thistle"
+    }
+])
 }
 
